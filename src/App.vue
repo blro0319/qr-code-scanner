5f3c1bab04f3049c073289fb87a06e7c4c83c92a
Blro <gyeongtae020319@gmail.com>
2023-10-04 23:17:54 +0900

    스캔 결과를 alert로 출력

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import type { Html5QrcodeResult } from "html5-qrcode";
 import QRCodeScanner from "./components/QRCodeScanner.vue";
+
+function handleScan(result: Html5QrcodeResult) {
+  alert(result.decodedText);
+}
 </script>
 
 <template>
-    <QRCodeScanner />
+  <QRCodeScanner @scan="handleScan" />
 </template>
 
 <style scoped></style>
